e1660cf3cfde5d4fe6a0044d9e4f674f1fe6132e
evan.sayer <evan.sayer@e6edf6fb-f266-4316-afb4-e53d95876a76>
2011-10-02 18:11:21 +0000

    git-svn-id: https://ideaeng.jira.com/svn/CSHARPSPIDER@716 e6edf6fb-f266-4316-afb4-e53d95876a76

---
+++ trunk/_SpiderConsoleApp.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Spider;
+
+namespace SpiderConsoleApp {
+
+    class SpiderConsoleApp {
+
+        static void Main(string[] args) {
+            string startUrl = "http://www.ideaeng.com";
+            string baseUrl = "http://www.ideaeng.com";
+            Spider.Spider s = new Spider.Spider(startUrl, baseUrl, 500);
+            
+            s.spider();
+
+            List<SpiderPage> results = s.getResults();
+            for (int i = 0; i < results.Count; i++) {
+                SpiderPage curr = results.ElementAt(i);
+                List<string> curr_links = curr.getLinkingToUrls();
+                List<string> curr_refs = curr.getReferencedByUrls();
+
+                System.Console.WriteLine("\t" + curr.getUrl() + " links to " + curr_links.Count + " page(s):");
+                for (int k = 0; k < curr_links.Count; k++) {
+                    System.Console.WriteLine("\t\t" + curr_links.ElementAt(k));
+                }
+
+                System.Console.WriteLine("\t" + curr.getUrl() + " is referred to by " + curr_refs.Count + " page(s):");
+                for (int g = 0; g < curr_refs.Count; g++) {
+                    System.Console.WriteLine("\t\t" + curr_refs.ElementAt(g));
+                }
+
+                System.Console.WriteLine("------------------------------------------------------------------------------------");
+            }
+        }
+    }
+}
