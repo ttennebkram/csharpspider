8b479d54c3553a1f810d9f2cc40e7e0e54229b3c
evan.sayer <evan.sayer@e6edf6fb-f266-4316-afb4-e53d95876a76>
2011-11-12 00:02:19 +0000

    Solr script changes
    git-svn-id: https://ideaeng.jira.com/svn/CSHARPSPIDER@825 e6edf6fb-f266-4316-afb4-e53d95876a76

--- branches/new-dev/SpiderConsoleApp/SpiderConsoleApp/bin/Debug/pages/post-to-solr.sh
+++ branches/new-dev/SpiderConsoleApp/SpiderConsoleApp/bin/Debug/pages/post-to-solr.sh
@@ -2,6 +2,7 @@
 
 c=0;
 for i in `ls | grep .html`; do
-	curl 	http://localhost:8983/solr/update/extract\?literal.id\=doc$c\&uprefix=attr_\&fmap.content=attr_content\&commit=true -F myfile=@$i;
+	echo "posting doc$c to Solr..."
+	curl http://localhost:8983/solr/update/extract\?literal.id\=doc$c\&uprefix=attr_\&fmap.content=attr_content\&commit=true -F myfile=@$i;
 	c=$(($c + 1));
 done
