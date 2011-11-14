7b5d90b5a56269927a6cd4173b85ab788ae84716
evan.sayer <evan.sayer@e6edf6fb-f266-4316-afb4-e53d95876a76>
2011-11-14 00:15:12 +0000

    Solr text field update
    git-svn-id: https://ideaeng.jira.com/svn/CSHARPSPIDER@828 e6edf6fb-f266-4316-afb4-e53d95876a76

--- branches/new-dev/SpiderConsoleApp/SpiderConsoleApp/bin/Debug/pages/post-to-solr.sh
+++ branches/new-dev/SpiderConsoleApp/SpiderConsoleApp/bin/Debug/pages/post-to-solr.sh
@@ -3,6 +3,6 @@
 c=0;
 for i in `ls | grep .html`; do
 	echo "posting doc$c to Solr..."
-	curl http://localhost:8983/solr/update/extract\?literal.id\=doc$c\&uprefix=attr_\&fmap.content=attr_content\&commit=true -F myfile=@$i;
+	curl http://localhost:8983/solr/update/extract\?literal.id\=doc$c\&uprefix=attr_\&fmap.content=text\&commit=true -F myfile=@$i;
 	c=$(($c + 1));
 done
